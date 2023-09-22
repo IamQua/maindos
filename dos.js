192187d958b8eb95bc6e4b572c9abfcd24efbd9b
IamQua <omnijberte@gmail.com>
2023-09-22 12:43:54 -0400

    initial commit

---
+++ dos.js
@@ -0,0 +1,74 @@
+
+
+
+const button = document.querySelector("button");
+
+window.addEventListener("scroll", () => {
+  const rect = button.getBoundingClientRect();
+  const windowHeight = window.innerHeight;
+  
+  if (rect.top < windowHeight && rect.bottom >= 0) {
+    button.style.transform = "scale(1.2)";
+  } else {
+    button.style.transform = "scale(1)";
+  }
+});
+
+
+
+// DOM Elements
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
+solarButton.onclick = () => {
+
+  if (body.classList.contains('solar')) {
+    
+    body.classList.remove('solar');
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
+
+    solarButton.innerText = 'Rogue';
+
+    localStorage.removeItem('isSolar');
+
+  } else {
+
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `
+
+    body.classList.add('solar');
+    solarButton.innerText = 'normalize';
+
+    localStorage.setItem('isSolar', true);
+  }
+};

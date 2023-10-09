03f2ce9206f84784c9ed07209211c64708290288
IamQua <omnijberte@gmail.com>
2023-10-09 16:02:18 -0400

    proje11

--- dos.js
+++ dos.js
@@ -70,17 +70,13 @@ solarButton.onclick = () => {
   }
 };
 
-
-// Write your code here
-// Get all the necessary DOM elements
 const form = document.getElementById('exampleForm')
 const submitButton = document.querySelector('.submit')
 const successMessage = document.getElementById('form-submitted-msg')
 
-// Store all form elements in an array by spreading the elements property of the form
+
 const formElements = [ ...form.elements ]
 
-// Create function to check if all form elements are valid
 const allInputsValid = () => {
   const valid = formElements.every((element) => {
     if (element.nodeName === 'SELECT') {
@@ -94,11 +90,9 @@ const allInputsValid = () => {
   return valid
 }
 
-// Define a function to handle changes to any form element
+
 const handleChange = () => {
-  // Use the forEach() function to execute the provided function once for each element in the formElements array
   formElements.forEach((element) => {
-    // If the element is invalid and is not a button, a select dropdown, a checkbox, or a radio button, style it with a red border and red text
     if (!element.checkValidity()
           && element.nodeName !== 'BUTTON'
           && element.nodeName !== 'SELECT'  
@@ -111,8 +105,6 @@ const handleChange = () => {
       element.previousElementSibling.style.color = 'orange'
     }
 
-    // If the element is valid, reset its style to the original colors
-    // The conditions are the same as above for excluding certain elements
     if (element.checkValidity()
           && element.nodeName !== 'BUTTON'
           && element.nodeName !== 'SELECT'
@@ -124,8 +116,6 @@ const handleChange = () => {
       element.nextElementSibling.style.display = 'none'
       element.previousElementSibling.style.color = '#19ec1b'
     }
-
-    // If the element is a checkbox or a radio button and is invalid, style it with a red border and red text
     if (!element.checkValidity()
           && (element.type === 'checkbox'
               || element.type === 'radio')
@@ -134,7 +124,6 @@ const handleChange = () => {
       element.nextElementSibling.style.color = 'orange'
     }
 
-    // If the checkbox or radio button is valid, reset its style to the original colors
     if (element.checkValidity()
           && (element.type === 'checkbox'
               || element.type === 'radio')
@@ -143,7 +132,6 @@ const handleChange = () => {
       element.nextElementSibling.style.color = '#19ec1b'
     }
 
-    // If the element is a select dropdown and the default option is selected, style it with a red border and red text
     if (element.nodeName === 'SELECT'
           && element.value === 'Please select an option'
     ) {
@@ -153,7 +141,6 @@ const handleChange = () => {
       element.previousElementSibling.style.color = 'orange'
     }
 
-    // If an option other than the default is selected in the dropdown, reset its style to the original colors
     if (element.nodeName === 'SELECT'
           && element.value !== 'Please select an option'
     ) {
@@ -164,35 +151,29 @@ const handleChange = () => {
     }
   })
 
-  // If all form elements are valid, enable the submit button; otherwise, disable it
   if (allInputsValid()) {
     submitButton.removeAttribute('disabled', '')
   } else {
     submitButton.setAttribute('disabled', '')
   }
 }
-// Define a function to handle form submission
-const handleSubmit = (e) => {
-  // Prevent the default form submission behavior
-  e.preventDefault()
 
-  // If all form elements are valid after the form submission, display a success message, reset the form, and disable the submit button
+const handleSubmit = (e) => {
+  e.preventDefault()
   if (allInputsValid()) {
     successMessage.style.display = 'block'
     form.reset()
     submitButton.setAttribute('disabled', '')
 
-    // Hide the success message after 3 seconds
+   
     setTimeout(() => {
       successMessage.style.display = 'none'
     }, 3000)
   }
 }
 
-// Add event listener to each form element
 formElements.forEach((element) => {
   element.addEventListener('change', handleChange)
 })
 
-// Add submit listener to the form
 form.addEventListener('submit', (e) => handleSubmit(e))
